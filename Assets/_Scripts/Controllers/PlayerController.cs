688da26b977c062e99378fb495b5df9de5bd6c0f
victoria <99865288+scifi-vic@users.noreply.github.com>
2022-04-22 16:23:54 -0700

    updated dash

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : BulletUnit
     public static PlayerController instance;
     public float lineDamage = 1f;
     public float boostFactor = 5f;
+    public float dashSpeed;
+    public float startDashTime;
+    public float buttonTimer = 0.4f;
+    public int buttonCount = 0; 
+    private float dashTime;
+  
     private bool boosting = false; 
     private void Awake()
     {
@@ -24,6 +30,7 @@ public class PlayerController : BulletUnit
     public override void Start()
     {
         base.Start(); // calls BulletUnit Start()
+        dashTime = startDashTime; 
     }
 
     // Update is called once per frame
@@ -46,25 +53,38 @@ public class PlayerController : BulletUnit
             rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, friction * Time.deltaTime);
         }
             
-        
+        buttonTimer -= Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.Space)){
-            Boost();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            buttonCount++;
+
+            if (buttonTimer > 0 && buttonCount == 1)
+            {
+                Dash();
+                buttonCount = 0; 
+                boosting = false;
+            } 
+            else {
+                buttonTimer = 0.5f;
+                buttonCount = 0; 
+            }
         }
 
-        if(Input.GetKeyUp(KeyCode.Space)){
-            boosting = false;
-        }
-           
         if (Input.GetMouseButton(0))
         {   
              bm.Spawn(transform.position, -transform.up);
         }
     }
 
-    void Boost(){
+    void Dash(){
         boosting = true;
-        rb.AddForce(-transform.up * boostFactor * movementSpeed);
-        //StartCoroutine(BoostCooldown());
+        //rb.AddForce(-transform.up * boostFactor * movementSpeed);
+        ////StartCoroutine(BoostCooldown());
+        
+        rb.velocity = rb.velocity + (-(Vector2)transform.up * dashSpeed * movementSpeed);
+
+
+        
     }
 }

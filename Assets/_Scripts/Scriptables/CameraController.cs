f35cb9d8bd06ee1090893bd5ba7e6b97383759ff
Ryan Pang <RyanPang12@gmail.com>
2022-04-21 22:29:56 -0600

    Added background with parrellax

---
+++ Assets/_Scripts/Scriptables/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform farBackground;
+    public Transform nearBackground;
+
+    public float smoothSpeed = 0.125f;
+
+    private float lastXPos;
+    private float lastYPos;
+
+    private void Start()
+    {
+        lastXPos = target.position.x;
+        lastYPos = target.position.y;
+    }
+
+    private void Update()
+    {   
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+       // Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        float amountToMoveX = transform.position.x - lastXPos;
+        float amountToMoveY = transform.position.y - lastYPos;
+        farBackground.position += new Vector3(amountToMoveX, amountToMoveY, 0);
+        nearBackground.position += new Vector3(amountToMoveX* .9f , amountToMoveY * .9f , 0);
+        lastXPos = transform.position.x;
+        lastYPos = transform.position.y;
+    }
+
+}

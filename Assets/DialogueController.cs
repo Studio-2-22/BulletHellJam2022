6ab96b7c4fd8de16afdf00c34f424fce90a3261b
Ryan Pang <RyanPang12@gmail.com>
2022-04-25 09:31:47 -0600

    Added dialogue

--- Assets/DialogueController.cs
+++ Assets/DialogueController.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class DialogueController : MonoBehaviour
 {
-    public float typingSpeed = 0.1f;
+    public float typingSpeed = 0.05f;
     public string[] dialogues;
     public string fullText;
     private string currentText = "";
@@ -21,12 +21,15 @@ public class DialogueController : MonoBehaviour
 
 
     IEnumerator ShowText(){
+        transform.GetChild(1).gameObject.SetActive(true);
         for (int i = 0; i < fullText.Length; i++){
             currentText = fullText.Substring(0, i);
-            this.GetComponent<TextMeshProUGUI>().text = currentText;
+            transform.GetChild(1).gameObject.GetComponentInChildren<TextMeshProUGUI>().text = currentText;
             yield return new WaitForSeconds(typingSpeed);
         }
         index++;
+        yield return new WaitForSeconds(5);
+        transform.GetChild(1).gameObject.SetActive(false);
         if (index < dialogues.Length){
             fullText = dialogues[index];
             yield return new WaitForSeconds(60);
